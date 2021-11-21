c654bdd58b07b40abfe308af4f196edaab15408e
eytanadler <eytana@umich.edu>
2021-11-21 14:26:47 -0500

    Added more detailed comment

--- examples/dark_mode.py
+++ examples/dark_mode.py
@@ -2,7 +2,9 @@
 ==============================================================================
 Dark mode plotting example
 ==============================================================================
-An example of dark mode usage applied to some of the existing examples.
+An example of dark mode usage applied to some of the existing examples. Shows
+various usage of the dark_mode and set_dark_background in
+niceplots.setRCParams().
 """
 
 # ==============================================================================
