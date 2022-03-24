34dd2c7b3578e30b7fdf05bf81f0a6920988e10a
Neil Wu <602725+nwu63@users.noreply.github.com>
2022-03-24 12:19:08 -0400

    update setup.py, set to v1.0.0

--- niceplots/__init__.py
+++ niceplots/__init__.py
@@ -1,2 +1,4 @@
+__version__ = "1.0.0"
+
 from .utils import *
 from .parula import *
--- setup.py
+++ setup.py
@@ -1,23 +1,30 @@
+from setuptools import setup
+import re
 import os
-import sys
-from distutils.core import setup
-from setuptools import find_packages
+
+__version__ = re.findall(
+    r"""__version__ = ["']+([0-9\.]*)["']+""",
+    open("niceplots/__init__.py").read(),
+)[0]
+
+this_directory = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(this_directory, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
 
 setup(
     name="niceplots",
-    version="0.1",
+    version=__version__,
     description="Plotting utilities for matplotlib in python",
-    long_description="""\
-""",
-    classifiers=[
-        "Development Status :: 2 - Pre-Alpha",
-    ],
+    long_description=long_description,
     keywords="",
-    author="John Jasa",
-    author_email="johnjasa@umich.edu",
+    author="",
+    author_email="",
     license="Apache License, Version 2.0",
     packages=[
         "niceplots",
     ],
-    install_requires=["numpy>=1.16", "matplotlib>=2.2"],
+    install_requires=[
+        "numpy>=1.16",
+        "matplotlib>=2.2",
+    ],
 )
