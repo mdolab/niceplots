aad3fbf79bfbace3029145f0c15382e140944bb9
Neil Wu <602725+nwu63@users.noreply.github.com>
2022-03-24 12:44:09 -0400

    clean up conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -8,5 +8,4 @@ extensions.extend(["sphinx_gallery.gen_gallery"])
 sphinx_gallery_conf = {
     "examples_dirs": "../examples",  # path to your example scripts
     "gallery_dirs": "auto_examples",  # path to where to save gallery generated output
-    # "filename_pattern": "bar_chart_example.py",  # only look at these files
 }
