430049b66838b17ac307762e187b7dbc221cc22e
Alasdair Gray <alachris@umich.edu>
2023-01-23 18:59:43 -0500

    Use svg images for example docs

--- doc/conf.py
+++ doc/conf.py
@@ -1,4 +1,13 @@
 from sphinx_mdolab_theme.config import *
+from sphinx_gallery.scrapers import matplotlib_scraper
+
+class matplotlib_svg_scraper(object):
+
+    def __repr__(self):
+        return self.__class__.__name__
+
+    def __call__(self, *args, **kwargs):
+        return matplotlib_scraper(*args, format='svg', **kwargs)
 
 # -- Project information -----------------------------------------------------
 project = "niceplots"
@@ -8,4 +17,5 @@ extensions.extend(["sphinx_gallery.gen_gallery"])
 sphinx_gallery_conf = {
     "examples_dirs": "../examples",  # path to your example scripts
     "gallery_dirs": "auto_examples",  # path to where to save gallery generated output
+    'image_scrapers': (matplotlib_svg_scraper(),),
 }
