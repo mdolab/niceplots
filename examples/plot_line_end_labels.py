96ac003a338d95763773a99b9ab8cc9f6965564f
Alasdair Gray <alachris@umich.edu>
2023-01-31 11:13:30 -0500

    Add `label_line_ends` and example (#32)
    * Add `label_line_ends` and example
    
    * Bump version
    
    * Cleanup example docstring
    
    * Make the function more better
    
    * explain which annotate keywords can't be added
    
    * Add x and y offset as inputs
    
    * Output annotations
    
    * typo
    
    * One last thing

---
+++ examples/plot_line_end_labels.py
@@ -0,0 +1,44 @@
+"""
+==============================================================================
+Line end labelling
+==============================================================================
+An example demonstrating the use of label_line_ends to
+automatically label the ends of lines nicely.
+"""
+
+# ==============================================================================
+# External Python modules
+# ==============================================================================
+import numpy as np
+import matplotlib.pyplot as plt
+import niceplots
+
+
+def plot_colored_lines(ax, n_lines=3):
+    """Plot lines with colors following the style color cycle.
+
+    Adapted from https://matplotlib.org/stable/gallery/style_sheets/style_sheets_reference.html
+    """
+    t = np.linspace(-10, 20, 100)
+
+    def sigmoid(t, t0):
+        return 1 / (1 + np.exp(-(t - t0)))
+
+    shifts = np.linspace(0, 10, n_lines)
+    amplitudes = np.linspace(1, 1.5, n_lines)
+    for t0, a in zip(shifts, amplitudes):
+        ax.plot(t, a * sigmoid(t, t0), "-", label=f"$t_0={t0}$", clip_on=False)
+    ax.set_xlim(-10, 20)
+
+
+plt.style.use(niceplots.get_style())
+
+fig, ax = plt.subplots()
+plot_colored_lines(ax)
+ax.set_xlabel("t")
+ax.set_ylabel("$\sigma$", rotation="horizontal", ha="right")
+niceplots.adjust_spines(ax)
+niceplots.label_line_ends(ax)
+
+fig.savefig("line_end_labels.svg")
+fig.savefig("line_end_labels.png")
--- niceplots/utils.py
+++ niceplots/utils.py
@@ -197,6 +197,74 @@ def draggable_legend(axis=None, color_on=True, **kwargs):
         legend[idx].draggable()
 
 
+def label_line_ends(ax, lines=None, labels=None, colors=None, x_offset_pts=6, y_offset_pts=0, **kwargs):
+    """Place a label just to the right of each line in the axes
+
+    Note: Because the labels are placed outside of the axes, this function works best for plots where all lines end as
+    close to the right edge of the axes as possible. Additionally you need to either use constrained_layout=True
+    (as NicePlots styles do), or call plt.tight_layout() after calling this function.
+
+    Parameters
+    ----------
+    ax : Matplotlib axes
+        axes to label the lines of
+    lines : iterable of matplotlib line objects, optional
+        Lines to label, by default all lines in the axes
+    labels : list of strings, optional
+        Labels for each line, by default uses each line's label attribute
+    colors : single or list of colors, optional
+        Color(s) to use for each line, can be a single color for all lines or a list containing an entry for each line,
+        by default uses each line's color
+    x_offset_pts : int, float, optional
+        Horizontal offset of label from the right end of the line, in points, by default 6
+    y_offset_pts : int, float, optional
+        Vertical offset of label from the right end of the line, in points, by default 0
+    **kwargs :
+        Any valid keywords for matplotlib's annotate function, except ``xy``, ``xytext``, ``color``, ``textcoords``,
+        ``va``
+
+    Returns
+    -------
+    list of matplotlib annotation objects
+        The annotations created
+    """
+    if lines is None:
+        lines = ax.get_lines()
+
+    if labels is None:
+        labels = [line.get_label() for line in lines]
+    numLines = len(lines)
+    if len(labels) != numLines:
+        raise ValueError(f"Number of labels ({len(labels)}) doesn't match number of lines ({numLines})")
+
+    if colors is None:
+        colors = [line.get_color() for line in lines]
+    if not isinstance(colors, list):
+        colors = [colors] * len(lines)
+    if len(colors) != numLines:
+        raise ValueError(f"Number of colors ({len(colors)}) doesn't match number of lines ({numLines})")
+
+    annotations = []
+
+    for line, label, color in zip(lines, labels, colors):
+        # Get the x, y coordinates of the right-most point on the line
+        maxXIndex = np.argmax(line.get_xdata())
+        x = line.get_xdata()[maxXIndex]
+        y = line.get_ydata()[maxXIndex]
+        annote = ax.annotate(
+            label,
+            xy=(x, y),
+            xytext=(x_offset_pts, y_offset_pts),
+            color=color,
+            textcoords="offset points",
+            va="center",
+            **kwargs,
+        )
+        annotations.append(annote)
+
+    return annotations
+
+
 def horiz_bar(labels, times, header, nd=1, size=[5, 0.5], color=None):
     """Creates a horizontal bar chart to compare positive numbers.
 
