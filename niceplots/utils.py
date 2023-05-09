6ceaaa399c1d92bf4c9203ed3bd8204a57e7832b
Eytan Adler <63426601+eytanadler@users.noreply.github.com>
2023-05-09 13:27:42 -0400

    Add ability to get colors from style that is not active (#35)
    * Add ability to get colors from style that is not active
    
    * Ran black
    
    * Update __init__.py
    
    ---------
    
    Co-authored-by: Alasdair Gray <alachris@umich.edu>

--- niceplots/utils.py
+++ niceplots/utils.py
@@ -53,10 +53,20 @@ def get_style(styleName="doumont-light"):
     return styleName
 
 
-def get_colors():
+def get_colors(styleName=None):
     """
     Get a dictionary with the colors for the current style. This function
     only works when niceplots styles are used (not built-in matplotlib ones).
+
+    Parameters
+    ----------
+    styleName : str, optional
+        Name of desired style. By default gets the colors for the current style. Avaiable styles are:
+
+            - doumont-light: the niceplots style you know and love
+            - doumont-dark: the dark version of the niceplots style you know and love
+            - james-dark: a really cool alternative to classic niceplots
+            - james-light: a version of james with a light background, naturally
 
     Returns
     -------
@@ -70,36 +80,58 @@ def get_colors():
             - "Text": default text color
             - "Label": axis label color
     """
-    # Get the color codes and their names from the (hopefully) "special" parameter
-    color_codes = get_colors_list()
-    color_names = plt.rcParams["keymap.help"]
 
-    # Ensure that the amount of color names matches the amount of colors
-    if len(color_codes) != len(color_names):
-        raise ValueError(
-            "The colors are not properly named in the stylesheet, please open an issue on GitHub with the details!"
-        )
+    def get_colors_from_current_style():
+        # Get the color codes and their names from the (hopefully) "special" parameter
+        color_codes = get_colors_list()
+        color_names = plt.rcParams["keymap.help"]
 
-    colors = OrderedDict(zip(color_names, color_codes))
-    colors["Axis"] = plt.rcParams["axes.edgecolor"]
-    colors["Background"] = plt.rcParams["axes.facecolor"]
-    colors["Text"] = plt.rcParams["text.color"]
-    colors["Label"] = plt.rcParams["axes.labelcolor"]
+        # Ensure that the amount of color names matches the amount of colors
+        if len(color_codes) != len(color_names):
+            raise ValueError(
+                "The colors are not properly named in the stylesheet, please open an issue on GitHub with the details!"
+            )
 
-    return colors
+        colors = OrderedDict(zip(color_names, color_codes))
+        colors["Axis"] = plt.rcParams["axes.edgecolor"]
+        colors["Background"] = plt.rcParams["axes.facecolor"]
+        colors["Text"] = plt.rcParams["text.color"]
+        colors["Label"] = plt.rcParams["axes.labelcolor"]
+
+        return colors
+
+    if styleName:
+        with plt.style.context(get_style(styleName)):
+            return get_colors_from_current_style()
+    else:
+        return get_colors_from_current_style()
 
 
-def get_colors_list():
+def get_colors_list(styleName=None):
     """
     Get a list with the colors for the current style. This function
     works with all matplotlib styles.
+
+    Parameters
+    ----------
+    styleName : str, optional
+        Name of desired style. By default gets the colors for the current style. Avaiable styles are:
+
+            - doumont-light: the niceplots style you know and love
+            - doumont-dark: the dark version of the niceplots style you know and love
+            - james-dark: a really cool alternative to classic niceplots
+            - james-light: a version of james with a light background, naturally
 
     Returns
     -------
     list
         List of the colors for the requested style.
     """
-    return plt.rcParams["axes.prop_cycle"].by_key()["color"]
+    if styleName:
+        with plt.style.context(get_style(styleName)):
+            return plt.rcParams["axes.prop_cycle"].by_key()["color"]
+    else:
+        return plt.rcParams["axes.prop_cycle"].by_key()["color"]
 
 
 def get_available_styles():
