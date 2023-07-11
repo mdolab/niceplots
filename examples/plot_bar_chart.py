be7668702eaf95ea07dabf2a605930547286eccf
Alasdair Gray <alachris@umich.edu>
2023-07-11 13:57:50 -0700

    `horiz_bar` fix (#37)
    * Add helper function for saving figure in multiple formats
    
    * Return all axes from `horiz_bar`
    
    * Bump patch version
    
    * Change `axarr` to `axes`

--- examples/plot_bar_chart.py
+++ examples/plot_bar_chart.py
@@ -19,5 +19,5 @@ times = [0.00456, 0.00847, 0.0110, 0.0213, 0.011]
 nd = 4
 
 with plt.style.context(niceplots.get_style()):
-    fig, ax = niceplots.horiz_bar(labels, times, header, nd=nd, size=[7, 0.65])
+    fig, axes = niceplots.horiz_bar(labels, times, header, nd=nd, size=[7, 0.65])
     niceplots.save_figs(fig, "bar_chart", ["png", "svg"])
