0f51c4e4e3c94a4cd44946c36d256b7f9ebf84b7
Alasdair Gray <alachris@umich.edu>
2025-02-28 16:53:29 -0500

    Add fonts on import

--- niceplots/__init__.py
+++ niceplots/__init__.py
@@ -1,4 +1,19 @@
+import os
+from matplotlib import font_manager
+
 __version__ = "2.5.1"
+
+
+def addFonts():
+
+    font_dirs = [os.path.join(os.path.dirname(__file__), "fonts")]
+    font_files = font_manager.findSystemFonts(fontpaths=font_dirs)
+
+    for font_file in font_files:
+        font_manager.fontManager.addfont(font_file)
+
+
+addFonts()
 
 from .utils import *
 from .parula import *
